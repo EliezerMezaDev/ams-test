d323b83f60f5311365126d701d09d6069bc34ca0
Eliezer A Meza <150059983+EliezerMezaDev@users.noreply.github.com>
2025-05-11 12:10:50 -0400

    qwe

--- src/routes/contacto/+page.svelte
+++ src/routes/contacto/+page.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+	export const prerender = true;
+
 	import { onMount } from 'svelte';
 	import { elementColors } from '$lib/store';
 	import ContactForm from '$lib/components/contact/ContactForm.svelte';
